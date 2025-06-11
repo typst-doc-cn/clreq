ceb04ac59924db0ae8988693291e219f1e2cd70e
Myriad-Dreamin <camiyoru@gmail.com>
2025-06-11 12:35:41 +0800

    docs: add docs to code

--- scripts/compile.mjs
+++ scripts/compile.mjs
@@ -1,20 +1,92 @@
 import { spawn } from "child_process";
 import fs from "fs/promises";
-
-// var watch = require("glob-watcher");
 import watch from "glob-watcher";
 
 const lightGreen = "\x1b[32m";
 const lightBlue = "\x1b[34m";
 const reset = "\x1b[0m";
+
+// todo cache, clean not used examples
+const exampleCache = {};
+
 const log = (line, id, color = lightGreen) => `${color}[${id}]${reset} ${line}`;
 const mainHint = (line) => log(line, "main", lightBlue);
-const prefixScreen = (buffer, id) => {
+
+const extraArgs = [
+  "--diagnostic-format",
+  "short",
+  "--features=html",
+  "--font-path=fonts",
+];
+
+await main();
+
+async function main() {
+  await fs.mkdir("dist/examples", { recursive: true });
+  await fs.mkdir("dist/assets", { recursive: true });
+
+  const compileOnce = async () => {
+    await renderExamples();
+    await typst("index", [
+      "compile",
+      "index.typ",
+      "dist/index.html",
+      ...extraArgs,
+    ]);
+  };
+
+  if (process.argv.includes("--watch") || process.argv.includes("-w")) {
+    console.log("Watching for changes...");
+
+    // We estimiate which files could affect the compilation
+    const glob = watch(["{index,main}.typ", "examples/**/*", "typ/**/*"], {
+      persistent: true,
+      ignorePermissionErrors: true,
+    });
+
+    glob.on("change", async (_event, path) => {
+      console.clear();
+      console.log(`File changed: ${path}`);
+      try {
+        await compileOnce();
+        console.log(mainHint("Recompiled successfully."));
+      } catch (error) {
+        console.error("Error during recompilation:", error);
+      }
+    });
+    console.clear();
+    await compileOnce();
+  } else {
+    try {
+      await compileOnce();
+      console.log("Compilation completed successfully.");
+    } catch (error) {
+      console.error("Error during compilation:", error);
+    }
+  }
+}
+
+/**
+ * Prefix each line of the buffer with the identifier and a color.
+ *
+ * @param {Buffer} buffer - The buffer containing the output from the typst command
+ * @param {string} id - Identifier for the task process, used for logging
+ * @returns {string} - The formatted string with each line prefixed by the identifier
+ */
+function prefixScreen(buffer, id) {
   const lines = buffer.toString("utf-8").trim().split("\n");
   return lines.map((line) => log(line, id)).join("\n");
-};
+}
 
-const typst = (id, args, opts = {}) => {
+/**
+ * Run a typst command with the given arguments.
+ *
+ * @param {string} id - Identifier for the task process, used for logging
+ * @param {string[]} args - Arguments to pass to the typst command
+ * @param {import ('node:child_process').SpawnOptions} opts - Options for the spawn command
+ * @returns {Promise<string>} - Resolves with the output of the typst command
+ */
+function typst(id, args, opts = {}) {
   return new Promise((resolve, reject) => {
     const proc = spawn("typst", args, opts);
 
@@ -31,22 +103,12 @@ const typst = (id, args, opts = {}) => {
       }
     });
   });
-};
+}
 
-await fs.mkdir("dist/examples", { recursive: true });
-await fs.mkdir("dist/assets", { recursive: true });
-
-const extraArgs = [
-  "--diagnostic-format",
-  "short",
-  "--features=html",
-  "--font-path=fonts",
-];
-
-// todo cache, clean not used examples
-const cache = {};
-
-const renderExamples = async () => {
+/**
+ * Render examples from the main.typ file and compile them into SVG files.
+ */
+async function renderExamples() {
   const examples = JSON.parse(
     await typst("query", [
       "query",
@@ -63,11 +125,11 @@ const renderExamples = async () => {
    */
   const compileExample = async (example) => {
     const { id, content } = example;
-    if (cache[id]) {
+    if (exampleCache[id]) {
       // todo: typ change may change the output, so we need to check if the content is the same
       // if already compiled, skip
       console.log(mainHint(`Example ${id} already compiled.`));
-      return cache[id].output;
+      return exampleCache[id].output;
     }
 
     // compiles and creates svg files
@@ -77,7 +139,7 @@ const renderExamples = async () => {
       const output = `dist/assets/${id}.svg`;
       await typst(id, ["compile", input, output, ...extraArgs]);
       console.log(mainHint(`Compiled example: ${id}`));
-      cache[id] = {
+      exampleCache[id] = {
         lifetime: 1,
         output,
       };
@@ -90,7 +152,7 @@ const renderExamples = async () => {
   await Promise.all(examples.map(compileExample));
 
   const snapshot = Object.fromEntries(
-    Object.entries(cache).map(([id, { output }]) => [id, { output }])
+    Object.entries(exampleCache).map(([id, { output }]) => [id, { output }])
   );
   const toWrite = JSON.stringify(snapshot, null, 2);
   const existing = await fs
@@ -103,45 +165,5 @@ const renderExamples = async () => {
       "content/snapshot/example.json.tmp",
       "content/snapshot/example.json"
     );
-  }
-};
-
-const compileOnce = async () => {
-  await renderExamples();
-  await typst("index", [
-    "compile",
-    "index.typ",
-    "dist/index.html",
-    ...extraArgs,
-  ]);
-};
-
-if (process.argv.includes("--watch") || process.argv.includes("-w")) {
-  console.log("Watching for changes...");
-
-  // We estimiate which files could affect the compilation
-  const glob = watch(["{index,main}.typ", "examples/**/*", "typ/**/*"], {
-    persistent: true,
-    ignorePermissionErrors: true,
-  });
-
-  glob.on("change", async (_event, path) => {
-    console.clear();
-    console.log(`File changed: ${path}`);
-    try {
-      await compileOnce();
-      console.log(mainHint("Recompiled successfully."));
-    } catch (error) {
-      console.error("Error during recompilation:", error);
-    }
-  });
-  console.clear();
-  await compileOnce();
-} else {
-  try {
-    await compileOnce();
-    console.log("Compilation completed successfully.");
-  } catch (error) {
-    console.error("Error during compilation:", error);
   }
 }
